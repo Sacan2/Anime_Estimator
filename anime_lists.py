72093a6991a413e62799ce344d3f01a6ebec5b38
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-03-02 18:48:55 +0100

    First Commit

---
+++ anime_lists.py
@@ -0,0 +1,9 @@
+import random
+
+konosuba = ["Kazuma", "Aqua", "Megumin", "Darkness"]
+chainsaw_man = ["Denji", "Makima", "Kobeni", "Pochita"]
+jujutsu_kaisen = ["Yuji", "Toji", "Satorou", "Todo"]
+mashle = ["Lancer", "Mash", "Finn", "Dot"]
+
+random_number = random.randint(0, 3)
+print(konosuba[random_number])
