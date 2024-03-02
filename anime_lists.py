af44fb4327ebb739069b5bc5c5eb59ea3f9481a1
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-03-02 19:40:52 +0100

    Second Commit nothing much just some first tries. Need to find a solution for the check that the user_input and selected_random_ability are correct.

--- anime_lists.py
+++ anime_lists.py
@@ -1,12 +1,30 @@
 import random
+from colorama import Fore, Back, Style
 
 konosuba = ["Kazuma", "Aqua", "Megumin", "Darkness"]
 chainsaw_man = ["Denji", "Makima", "Kobeni", "Pochita"]
 jujutsu_kaisen = ["Yuji", "Toji", "Satorou", "Todo"]
 mashle = ["Lancer", "Mash", "Finn", "Dot"]
 
-random_number = random.randint(0, 3)
-print(konosuba[random_number])
-print(konosuba[random_number])
-print(konosuba[random_number])
-print(konosuba[random_number])
+random_character = random.randint(0, 3)
+random_ability = random.randint(0, 2)
+
+abilities = ["STEALLLLL", "Black Flash", "Boogie Woogie"]
+
+selected_random_ability = abilities[random_ability]
+
+print(Fore.GREEN + selected_random_ability)
+print(Style.RESET_ALL)
+
+user_input = input("Which Character is it?")
+
+match user_input:
+    case "":
+        print("You failed")
+
+if selected_random_ability == "STEALLLLL":
+    print("Kazuma")
+elif selected_random_ability == "Black Flash":
+    print("Yuji")
+elif selected_random_ability == "Boogie Woogie":
+    print("Todo")
