36e963c0ae9e0db1cce36cdca5d2f451312fef00
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-03-18 22:36:31 +0100

    Dictionary wurde durch ein Array ersetzt. Ich habe Dictory benutzt, um die Namen zu speichern. Habe dafür das Array entfernt.

---
+++ anime_lists.py
@@ -0,0 +1,11 @@
+chainsaw_man = {
+    "Denji": "Chainsaw",
+    "Makima": "Bang",
+    "Power": "Blood Hammer"
+}
+
+jujutsu_kaisen = {
+    "Yuji": "Fivergent fist",
+    "Gojo": "Hollow Pruple",
+    "Megumin": "Ten Shadow"
+}
--- main.py
+++ main.py
@@ -1,43 +1,14 @@
+import anime_lists
 import random
-from colorama import Fore, Back, Style
 
-konosuba = ["Kazuma", "Aqua", "Megumin", "Darkness"]
-chainsaw_man = ["Denji", "Makima", "Kobeni", "Pochita"]
-jujutsu_kaisen = ["Yuji", "Toji", "Gojo", "Todo"]
-mashle = ["Lancer", "Mash", "Finn", "Dot"]
+character_numbers = len(anime_lists.jujutsu_kaisen)
 
-random_character = random.randint(0, 3)
-random_ability = random.randint(0, 2)
+print(character_numbers)
 
-abilities = ["Hollow Purple", "Divergent fist", "Boogie Woogie", "heavenly restriction"]
+random_character_number = random.randrange(0, character_numbers)
 
-selected_random_ability = abilities[random_ability]
+print(random_character_number)
 
-print(Fore.GREEN + selected_random_ability)
-print(Style.RESET_ALL)
+print(list(anime_lists.jujutsu_kaisen.keys())[random_character_number])
+print(list(anime_lists.jujutsu_kaisen.values())[random_character_number])
 
-for x in jujutsu_kaisen:
-    print(x)
-
-game_start = True
-while game_start:
-    user_input = input("Welcher Charakter ist es?\n")
-
-    match user_input.lower():
-        case "yuji" if selected_random_ability == "Divergent fist":
-            game_start = False
-            print("Du hast gewonnen")
-
-        case "gojo" if selected_random_ability == "Hollow Purple":
-            game_start = False
-            print("Du hast gewonnen")
-
-        case "todo" if selected_random_ability == "Boogie Woogie":
-            game_start = False
-            print("Du hast gewonnen")
-
-        case "toji" if selected_random_ability == "heavenly restriction":
-            game_start = False
-            print("Du hast gewonnen")
-        case _:
-            print("Du bist kacke")
